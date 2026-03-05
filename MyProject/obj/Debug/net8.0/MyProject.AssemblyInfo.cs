debd56c64c7078b630bcfe5d46e6732831013f5b
jack0310 <jack2780jack@gmail.com>
2026-03-05 15:02:04 +0900

    .

--- MyProject/obj/Debug/net8.0/MyProject.AssemblyInfo.cs
+++ MyProject/obj/Debug/net8.0/MyProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1242f7dcadbe72af770f16798191c43ee030f747")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0cc09305bba2527dae76627f91cefb5280fdc44")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
